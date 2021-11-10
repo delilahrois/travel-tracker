bf89387633445db379a56175914292a57011aba4
Delilah Necrason <delilahnecrason@gmail.com>
2021-11-10 16:50:50 -0500

    Add first api calls and imports

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,10 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import { fetchTravelers, fetchSingleTraveler, fetchTrips, 
+  fetchDestinations } from './apiCalls';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
