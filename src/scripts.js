057b6c9efb0048ce64b28b41195a18a39231d4d0
Delilah Necrason <delilahnecrason@gmail.com>
2021-11-10 17:15:13 -0500

    Get first fetch working

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,44 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+// Imports 
 
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 import { fetchTravelers, fetchSingleTraveler, fetchTrips, 
   fetchDestinations } from './apiCalls';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+// An example of how you tell webpack to use an image 
+// (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 
+// Global Variables
+
+let traveler;
+let travelerRepo;
+let trips;
+let destinations;
+
+
+// Query Selectors
+
+
+
+
+// Functions
+
+const fetchData = () => {
+  Promise.all([ fetchTravelers(), fetchTrips(), fetchDestinations() ]).then(data => {
+    return Promise.all(data.map(result => result.json()));
+  }).then(data => {
+    console.log(data)
+    // call functions to instantiate new instances of class here
+  })
+}
+
+
+
+// const createTravelers = (data) => {
+//   travelerRepo = new TravelerRepository(data); 
+// }
+
+// Event Listeners 
+
+window.addEventListener('load', fetchData);
