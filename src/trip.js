4207b7a5c885d124c39362be813c565e19baa706
Delilah Necrason <delilahnecrason@gmail.com>
2021-11-10 21:08:15 -0500

    Add trip test and class files

---
+++ src/trip.js
@@ -0,0 +1,14 @@
+class Trip {
+  constructor(tripData) {
+    this.id = tripData.id;
+    this.userID = tripData.userID;
+    this.destinationID = tripData.destinationID;
+    this.travelers = tripData.travelers;
+    this.date = tripData.date;
+    this.duration = tripData.duration;
+    this.status = tripData.status;
+    this.suggestedActivities = tripData.suggestedActivities;
+  }
+}
+
+module.exports = Trip;
